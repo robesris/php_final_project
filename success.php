ed35c1dd80858a7ee8914244edfee8d90e63e519
Rob Esris <robesris@salamander.local>
2010-06-29 13:14:52 -0400

    Added success page.

---
+++ success.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="./styles.css">
+  </head>
+  
+  <body>
+    <?php include './header.inc'; ?>
+    
+    <div class="bold">Your file has been uploaded successfully.</div>
+    
+    
+    <?php include './footer.inc'; ?>
+  </body>
+</html>
