2779590ff06af917ecb3985a5ff8175cb0bab737
Rob Esris <robesris@salamander.local>
2010-06-29 13:15:38 -0400

    Append numbered suffix to duplicate filenames to avoid overwriting exisiting files.

--- upload.php
+++ upload.php
@@ -1,20 +1,31 @@
 <?php
   require_once './common.inc';
+  require_once './constants.inc';
   
   session_start();
-  if ($_SERVER['REQUEST_METHOD'] == 'POST'):
-    if ($_FILES['file_ul']['tmp_name']):
+  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if ($_FILES['file_ul']['tmp_name']) {
       $file = $_FILES['file_ul'];
       $tmp = $file['tmp_name'];
-      $name = $file['name'];
-      if (move_uploaded_file($tmp, "./files/$name")): ?>
-        <div>You successfully uploaded "<?php print($name); ?>".</div>
-      <?php else: ?>
-        <div>File did not upload successfully!</div>
-      <?php endif;
-    endif;
-  endif;
-  // logout();
+      $basename = preg_replace('/[^A-Za-z0-9_]/', '_', $file['name']);
+      $name = $basename;
+      $dupnum = 0;
+      while (file_exists(UPLOADDIR . "/$name")) {
+        $dupnum++;
+        $name = $basename . "_$dupnum";
+      }
+      if (move_uploaded_file($tmp, UPLOADDIR . "/$name")) {
+        $_SESSION['upload_ok'] = true;
+        $_SESSION['upload_name'] = $name;
+        header("Location: " . dirname($_SERVER['PHP_SELF']) . "/success.php");
+        exit();
+      } else {
+        $_SESSION['upload_error_msg'] = "Your file did NOT upload successfully!";
+        header("Location: " . $_SERVER['PHP_SELF']);
+        exit();
+      }
+    }
+  }
 ?>
 <head>
   <link rel="stylesheet" type="text/css" href="./styles.css">
@@ -22,6 +33,10 @@
 
 <body>  
   <?php include './header.inc'; ?>
+  <?php if ($_SESSION['upload_error_msg']): ?>
+    <div class="error"><?php echo $_SESSION['upload_error_msg']; ?></div>
+    <?php $_SESSION['upload_error_msg'] = ""; ?>
+  <?php endif ?>
   <div id="upload_form">
     <form enctype="multipart/form-data" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
       <div class="question">
