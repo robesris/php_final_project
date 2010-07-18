e3bb6d372cc4fea524b9d930b25e55a76aa5566a
Rob Esris <robesris@salamander.local>
2010-07-17 20:09:54 -0400

    Modified upload page to use Smarty template.

--- upload.php
+++ upload.php
@@ -1,8 +1,9 @@
 <?php
+  session_start();
   require_once './common.inc';
+  require_once('./smarty_connect.php');
   require_once './constants.inc';
   
-  session_start();
   if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if ($_FILES['file_ul']['tmp_name']) {
       $file = $_FILES['file_ul'];
@@ -24,31 +25,16 @@
         header("Location: " . $_SERVER['PHP_SELF']);
         exit();
       }
+    } else {
+      $_SESSION['upload_error_msg'] = "Your file did NOT upload successfully!";
+      header("Location: " . $_SERVER['PHP_SELF']);
+      exit();
     }
   }
-?>
-<head>
-  <link rel="stylesheet" type="text/css" href="./styles.css">
-</head>
-
-<body>  
-  <?php include './header.inc'; ?>
-  <?php if ($_SESSION['upload_error_msg']): ?>
-    <div class="error"><?php echo $_SESSION['upload_error_msg']; ?></div>
-    <?php $_SESSION['upload_error_msg'] = ""; ?>
-  <?php endif ?>
-  <div id="upload_form">
-    <form enctype="multipart/form-data" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
-      <div class="question">
-        <label for="file_ul">Choose a file to upload.</label>
-        <div>
-          <input type="hidden" name="MAX_FILE_SIZE" value="1000000" />
-          <input type="file" name="file_ul" id="file_ul" />
-        </div>
-      </div>
   
-      <input type="submit" name="submit" value="Upload" />
-    </form>
-  </div>
-  <?php include './footer.inc'; ?>
-</body>
+  if ($_SESSION['upload_error_msg']) {
+    $smarty->assign('upload_error_div', "<div class='error'>{$_SESSION['upload_error_msg']}</div>");
+    $_SESSION['upload_error_msg'] = "";
+  }
+  $smarty->display(TDIR . 'upload.tpl');
+?>
