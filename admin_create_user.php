bf6f7b98f01ed30326361adbab0886272315659e
Rob Esris <robesris@salamander.local>
2010-07-20 00:07:13 -0400

    Implemented user creation.

---
+++ admin_create_user.php
@@ -0,0 +1,43 @@
+<?php
+  session_start();
+  require_once './common.inc';
+  require_once('./smarty_connect.php');
+  require_once './constants.inc';
+  require_once './database.inc';
+  
+  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $login = $_POST['login'];
+    $pw1   = $_POST['pw1'];
+    $pw2   = $_POST['pw2'];
+    if ($login && $pw1 && $pw2) {
+      if ($pw1 == $pw2) {
+        $pw_hash = md5($pw1);
+        $db = open_db();
+        $result = $db->query("SELECT login FROM users WHERE login='$login';");
+        if (!$result->fetch_row()) {
+          # Unique login and matching password - go ahead and create user
+          $perms = $_POST['perms'] ? $_POST['perms'] : DOWNLOAD;
+          $sql = $db->prepare("INSERT INTO users (login, perms, pass_hash) VALUES ('$login', $perms, '$pw_hash');");
+          $sql->execute();
+          $_SESSION['msg'] = "User created!";
+          header('Location: ' . $_SERVER['PHP_SELF']);
+          exit();
+        } else {
+          $error_msg = "Username already taken.";
+        }
+      } else {
+        $error_msg = "Passwords did not match.";
+      }
+    } else {
+      $error_msg = "Please fill in all required fields.";
+    }
+    $_SESSION['error_msg'] = $error_msg;
+    header('Location: ' . $_SERVER['PHP_SELF']);
+    exit();
+  }
+  
+  $smarty->assign('error_msg', $_SESSION['error_msg']);
+  $smarty->assign('msg', $_SESSION['msg']);
+  $_SESSION['error_msg'] = '';
+  $_SESSION['msg'] = '';
+  $smarty->display(TDIR . 'admin_create_user.tpl');
---
+++ templates/admin_create_user.tpl
@@ -0,0 +1,30 @@
+<head>
+  <link rel="stylesheet" type="text/css" href="./styles.css">
+</head>
+
+<body>
+  {include file=$header}
+  <div class="error">{$error_msg}</div>
+  <div class="success">{$msg}</div>
+
+  <div class="label">Create User</div>
+
+  <form action="{$smarty.server.PHP_SELF}" method="POST">
+    <div class="row">
+      <div class="col">Login</div>
+      <div><input type="text" name="login"></div>
+    
+      <div class="col">Password</div>
+      <div><input type="text" name="pw1"></div>
+    
+      <div class="col">Retype Password</div>
+      <div><input type="text" name="pw2"></div>
+    
+      <div class="col">Permissions</div>
+      <div><input type="text" name="perms"></div>
+    </div>
+    <div><input type="Submit" value="Create" /></div>
+  </form>
+
+  {include file=$footer}
+</body>
